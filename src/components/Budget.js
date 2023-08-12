72f933c0ae296243b965cf5fb5434f3a68db2ba9
Mohammad Harouak <M.harouak@aui.ma>
2023-08-12 16:30:34 +0100

    Update Budget.js

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,11 +1,35 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
+
 const Budget = () => {
-    const { budget } = useContext(AppContext);
-    return (
-        <div className='alert alert-secondary'>
-            <span>Budget: £{budget}</span>
-        </div>
-    );
+  const { budget, setBudget, expenses, currency } = useContext(AppContext);
+  const [editableBudget, setEditableBudget] = useState(budget);
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total = total + item.cost);
+  }, 0);
+  const handleBudgetChange = (newBudget) => {
+    if (newBudget >= totalExpenses) {
+      setEditableBudget(newBudget);
+      setBudget(newBudget);
+    } else {
+        alert("You cannot reduce the budget lower than the spending");
+        setBudget("");
+    }
+  };
+
+  return (
+    <div className='alert alert-secondary'>
+      <span>
+        Budget: {currency}
+        <input
+          type='number'
+          value={editableBudget}
+          onChange={e => handleBudgetChange(parseInt(e.target.value))}
+          step={10}
+        />
+      </span>
+    </div>
+  );
 };
+
 export default Budget;
